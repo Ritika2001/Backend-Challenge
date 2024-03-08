2eec89631359c452bc239117c5fe257a545a8bb0
Ritika Nandi <ritika.nandi77@gmail.com>
2024-03-07 23:59:48 -0500

    added caching and limiting

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,12 +4,19 @@ import { createQuote } from './controllers/quote.controller';
 import { getBestThreeQuotes } from './controllers/user.controller';
 import "reflect-metadata"
 import path from 'path';
+import cacheHeaders from 'express-cache-headers'; 
 
 
 const app = express();
 const port = 3000;
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, 
+  max: 50, 
+});
 
+app.use(limiter);
 const router = express.Router();
 app.use(cors());
 app.use(express.json());
@@ -27,8 +34,8 @@ myDataSource
       res.sendFile(path.join(__dirname, 'public', 'index.html'));
     });
 
-    app.post('/api/quotes', createQuote);
-    app.get('/api/quotes/best-three', getBestThreeQuotes);
+    app.post('/api/quotes', cacheHeaders(60), createQuote);
+    app.get('/api/quotes/best-three', cacheHeaders(60), getBestThreeQuotes);
 
     app.listen(port, () => {
       console.log(`Express is listening at http://localhost:${port}`);
